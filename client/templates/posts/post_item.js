5cd83b5754eea0a758836f5a821f8078c55489bd
Kenneth Yee <Kenneth_yee2@yahoo.com>
2016-05-12 09:46:04 -0400

    like button working

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -8,22 +8,40 @@ Template.postItem.helpers({
 });
 
 Template.postItem.events({
+	//remove this click event
 	'click .likeButton' : function(e){
 		e.preventDefault();
 
-		let likeCounter = 0
+		// var post = {
+		// 	totalLikes: 'helo'
+		// };
+
+		
 		if (Meteor.user()){
-			likeCounter++
-			alert(likeCounter);
+			
+			console.log(User.find());
+			console.log(this.userId);
+			console.log(Posts.find());
 		} else {
 			alert('You need to be logged in to like that post');
 		}
-	// Meteor.call('likePost');
+		
+		Meteor.call('likePost', function(error, result) {
+		if (error)
+			return alert(error.reason);
+
+	});
+	},
+	'click .upvote': function(e) {
+		e.preventDefault();
+		Meteor.call('upvote', this._id);
 	}
+
 });
 
 //if client clicks on "like" button..
-//total likes for post will increase by one
+
 
 //server side changes happening
-//
+//total likes of a certain post will increase by one 
+//user 
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,19 +1,90 @@
+// if (Posts.find().count() === 0) {
+// 	Posts.insert({
+// 		title: 'Introducing Telescope',
+// 		url: 'http://sachagreif.com/introducing-telescope/',
+// 		content: 'Great site!'
+// 	});
+
+// 	Posts.insert({
+// 		title: 'Meteor',
+// 		url: 'http://meteor.com',
+// 		content: 'Superb!'
+// 	});
+
+// 	Posts.insert({
+// 		title: 'The Meteor Book',
+// 		url: 'http://themeteorbook.com',
+// 		content: "m3teor"
+// 	});
+// }
+
+// Fixture data
 if (Posts.find().count() === 0) {
-	Posts.insert({
-		title: 'Introducing Telescope',
-		url: 'http://sachagreif.com/introducing-telescope/',
-		content: 'Great site!'
-	});
+var now = new Date().getTime();
 
+// create two users
+var kenId = Meteor.users.insert({
+	profile: { name: 'Kenneth Yee' }
+});
+var ken = Meteor.users.findOne(kenId);
+var benId = Meteor.users.insert({
+	profile: { name: 'Ben Smith' }
+});
+var ben = Meteor.users.findOne(benId);
+var telescopeId = Posts.insert({
+	title: 'Introducing Telescope',
+	userId: ben._id,
+	author: ben.profile.name,
+	url: 'http://sachagreif.com/introducing-telescope/',
+	submitted: new Date(now - 7 * 3600 * 1000),
+	commentsCount: 2,
+	upvoters: [],
+	votes: 0
+});
+Comments.insert({
+	postId: telescopeId,
+	userId: ken._id,
+	author: ken.profile.name,
+	submitted: new Date(now - 5 * 3600 * 1000),
+	body: 'Interesting project ben, can I get involved?'
+});
+Comments.insert({
+	postId: telescopeId,
+	userId: ben._id,
+	author: ben.profile.name,
+	submitted: new Date(now - 3 * 3600 * 1000),
+	body: 'You sure can ken!'
+});
+Posts.insert({
+	title: 'Meteor',
+	userId: ken._id,
+	author: ken.profile.name,
+	url: 'http://meteor.com',
+	submitted: new Date(now - 10 * 3600 * 1000),
+	commentsCount: 0,
+	upvoters: [],
+	votes: 0
+});
+Posts.insert({
+	title: 'The Meteor Book',
+	userId: ken._id,
+	author: ken.profile.name,
+	url: 'http://themeteorbook.com',
+	submitted: new Date(now - 12 * 3600 * 1000),
+	commentsCount: 0,
+	upvoters: [],
+	votes: 0
+});
+for (var i = 0; i < 10; i++) {
 	Posts.insert({
-		title: 'Meteor',
-		url: 'http://meteor.com',
-		content: 'Superb!'
+		title: 'Test post #' + i,
+		author: ben.profile.name,
+		userId: ben._id,
+		url: 'http://google.com/?q=test-' + i,
+		submitted: new Date(now - i * 3600 * 1000 + 1),
+		commentsCount: 0,
+		upvoters: [],
+		votes: 0
 	});
-
-	Posts.insert({
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com',
-		content: "m3teor"
-	});
+	}
 }
